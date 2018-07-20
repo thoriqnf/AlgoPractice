bbe638de2341b61d96c6e43b0afe6ee7d5e9691d
thoriq <thoriqnfaizal@gmail.com>
2018-07-20 18:04:02 +0700

    1st question, 2nd solution using for loop

--- reversestring/index.js
+++ reversestring/index.js
@@ -7,10 +7,21 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str
-    .split('')
-    .reverse()
-    .join('');
+  let reverse = '';
+
+  for (let characters of str) {
+    reverse = characters + reverse;
+  }
+
+  return reverse;
 }
 
 module.exports = reverse;
+
+// 1st solution (3 steps)
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
