5b1dcbb8b62a4155826bf9b39a7a93a976a972d8
thoriq <thoriqnfaizal@gmail.com>
2018-07-21 13:51:24 +0700

    2nd question, 2nd solution Using comparing first character with last character

--- palindrome/index.js
+++ palindrome/index.js
@@ -8,12 +8,19 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  const reversed = str
-    .split('')
-    .reverse()
-    .join('');
-
-  return str === reversed;
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+// 1st Solution
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return str === reversed;
+// }
