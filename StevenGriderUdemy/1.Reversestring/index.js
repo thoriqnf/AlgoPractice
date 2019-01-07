86d8f2dec0959fb5afac32bcdfec24126c4cf7b4
thoriq <thoriqnfaizal@gmail.com>
2019-01-07 16:57:52 +0700

    Adding 3rd solution for reverse string

--- StevenGriderUdemy/1.Reversestring/index.js
+++ StevenGriderUdemy/1.Reversestring/index.js
@@ -6,37 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str
-    .split("")
-    .reverse()
-    .join("");
-}
-
-module.exports = reverse;
-
-// 1st solution (3 steps)
+// 1st Solution (3 steps straight forward)
 // function reverse(str) {
 //   return str
-//     .split('')
+//     .split("")
 //     .reverse()
-//     .join('');
+//     .join("");
 // }
 
 // .reverse hanya bisa untuk array dan tak bisa untuk string makanya di awal di split dulu untuk chunk dari string ke array dan di sambungkan lagi pakai .join
 
-// 2nd solution (using for loop)
+// 2nd solution (for loop)
 // function reverse(str) {
-//   let reverse = '';
-
-//   for (let characters of str) {
-//     reverse = characters + reverse;
+//   let reversed = '';
+//   for (let character of str ){
+//     reversed = character + reversed;
 //   }
-
-//   return reverse;
+//   return reversed;
 // }
 
-// 3rd Solution using reduce
-// function reverse(str) {
-//   return str.split('').reduce((rev, char) => char + rev, '');
+// 3rd Solution (reduce)
+
+// function reversed(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
 // }
+
+// reduce is function es 5.1, bikin array jadi single value
+
+// module.exports ini gunanya ngexport nama functionnya
+
+module.exports = reversed;
