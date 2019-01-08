0a8d5e628ad0d3bb1c81496a2c9da672c41d9809
thoriq <thoriqnfaizal@gmail.com>
2019-01-08 16:17:18 +0700

    Adding solution for fizzbuzz

--- StevenGriderUdemy/5.Fizzbuzz/index.js
+++ StevenGriderUdemy/5.Fizzbuzz/index.js
@@ -12,31 +12,16 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {
-  for (let i = 1; i<= n; i++){
-    if(i & 3 === 0 && i % 5 === 0){
-      console.log('fizzbuzz');
-    } else if (i % 3 === 0){
-      console.log('fizz');
-    } else if (i % 5 === 0){
-      console.log('buzz');
-    }else {
-      console.log(i)
-    }
-  }
-}
-
+// Cetak semua number pas kelipatan 3 ketik fizz pas ketikan ke 5 keluarin buzz pas kelipatan 3 dan 5 fizzbuzz
 
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    // Is the number a multiple of 3 and 5?
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzbuzz');
+      console.log("fizzbuzz");
     } else if (i % 3 === 0) {
-      // Is the number a multiple of 3?
-      console.log('fizz');
+      console.log("fizz");
     } else if (i % 5 === 0) {
-      console.log('buzz');
+      console.log("buzz");
     } else {
       console.log(i);
     }
