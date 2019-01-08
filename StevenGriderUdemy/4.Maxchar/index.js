df16256a6d6e0034d0cfe1c3ec0a72bb4c51a9cd
thoriq <thoriqnfaizal@gmail.com>
2019-01-08 13:54:49 +0700

    Adding solution for maxcharacter

---
+++ StevenGriderUdemy/4.Maxchar/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+// Max char cari character yang paling sering keluar string ataupun integer
+// Caranya cari dulu berapa jumlahnya tiap character bisa lewat if atau ternary
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+module.exports = maxChar;
