b6b0337130c50d962368e1b186fd6ddc9aa95f4c
thoriq <thoriqnfaizal@gmail.com>
2019-01-15 21:24:41 +0700

    Adding 2nd solution for anagrams

--- StevenGriderUdemy/7.Anagrams/index.js
+++ StevenGriderUdemy/7.Anagrams/index.js
@@ -8,29 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// 1st Soluction
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
+
+// 2nd Solution
+
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
+  return cleanString(strinaA) === cleanString(stringB);
 }
 
-function buildCharMap(str) {
-  const charMap = {};
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-  return charMap;
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLoweCase()
+    .split("")
+    .sort()
+    .join("");
 }
+// Number can sort but string can sorted too from alphabets
+// Dia mau compare hasil sortingan dari alphabetsnya, kalo sama persis berarti anagramsnya sama
 
 module.exports = anagrams;
